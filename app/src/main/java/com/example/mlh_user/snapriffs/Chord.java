3c77056ffdc5a50239e0917b00779ffc56d9ad87
Joseph Buchoff <joseph.buchoff@rutgers.edu>
2016-01-17 13:48:01 -0500

    Edited inital silence out of chord audio. Fully functional!

--- app/src/main/java/com/example/mlh_user/snapriffs/Chord.java
+++ app/src/main/java/com/example/mlh_user/snapriffs/Chord.java
@@ -1,8 +1,40 @@
 package com.example.mlh_user.snapriffs;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 /**
- * Created by MLH-User on 1/16/2016.
+ * Created by Joseph Buchoff on 1/16/2016.
  */
 public class Chord {
+    // Name of the chord
+    public String name;
 
+    // MediaPlayer to play the chord
+    MediaPlayer chordPlayer;
+
+    // The int for the chord audio resource
+    int chordAudio;
+
+    /*
+    General constructor for Chord
+     */
+    public Chord(String name, int chordAudio, Context context)
+    {
+        this.name = name;
+        this.chordAudio = chordAudio;
+
+        // Initialize the chord player
+        chordPlayer = MediaPlayer.create(context, chordAudio);
+    }
+
+    /*
+    Takes in the context for the calling environment
+        and plays the chord
+     */
+    public void playChord(Context context)
+    {
+        if (!chordPlayer.isPlaying())
+            chordPlayer.start();
+    }
 }
--- app/src/main/java/com/example/mlh_user/snapriffs/MainActivity.java
+++ app/src/main/java/com/example/mlh_user/snapriffs/MainActivity.java
@@ -5,6 +5,7 @@ import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,32 +27,34 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     // Initialize the chords
-    private final Chord Am = new Chord("Am", R.raw.Am_chord);
-    private final Chord A = new Chord("A", R.raw.A_chord);
-    private final Chord C = new Chord("C", R.raw.C_chord);
-    private final Chord D = new Chord("D", R.raw.D_chord);
-    private final Chord Em = new Chord("Em", R.raw.Em_chord);
-    private final Chord E = new Chord("E", R.raw.E_chord);
-    private final Chord F = new Chord("F", R.raw.F_chord);
-    private final Chord G = new Chord("G", R.raw.G_chord);
+    private Chord Am;
+    private Chord A;
+    private Chord C;
+    private Chord D;
+    private Chord Em;
+    private Chord E;
+    private Chord F;
+    private Chord G;
+
+    private final int numChords = 8;
 
     // An array of all the chords
-    private final Chord[] chords = {Am, A, C, D, E, Em, F, G};
+    private Chord[] chords = new Chord[numChords];
 
     // Declare the selected chords for each fret
     private Chord selectedChord1, selectedChord2,selectedChord3, selectedChord4;
-
-    // Initialize the spinners
-    private Spinner s1 = (Spinner) findViewById(R.id.spinner);
-    private Spinner s2 = (Spinner) findViewById(R.id.spinner2);
-    private Spinner s3 = (Spinner) findViewById(R.id.spinner3);
-    private Spinner s4 = (Spinner) findViewById(R.id.spinner4);
 
     // isClicked booleans for buttons defined
     private boolean isClicked1 = false;
     private boolean isClicked2 = false;
     private boolean isClicked3 = false;
     private boolean isClicked4 = false;
+
+    /* Declaring Spinners
+    private Spinner s1;
+    private Spinner s2;
+    private Spinner s3;
+    private Spinner s4; */
 
     private Handler mHandler = new Handler();
 
@@ -109,11 +112,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the spinners
+        final Spinner s1 = (Spinner) findViewById(R.id.spinner);
+        final Spinner s2 = (Spinner) findViewById(R.id.spinner2);
+        final Spinner s3 = (Spinner) findViewById(R.id.spinner3);
+        final Spinner s4 = (Spinner) findViewById(R.id.spinner4);
+
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
         final Context context = getApplicationContext();
+
+        // Initialize the chords
+        Am = new Chord("Am", R.raw.am_chord, context);
+        A = new Chord("A", R.raw.a_chord, context);
+        C = new Chord("C", R.raw.c_chord, context);
+        D = new Chord("D", R.raw.d_chord, context);
+        Em = new Chord("Em", R.raw.em_chord, context);
+        E = new Chord("E", R.raw.em_chord, context);
+        F = new Chord("F", R.raw.f_chord, context);
+        G = new Chord("G", R.raw.g_chord, context);
+
+        // Initialize the array of all the chords
+        chords[0] = Am;
+        chords[1] = A;
+        chords[2] = C;
+        chords[3] = D;
+        chords[4] = Em;
+        chords[5] = E;
+        chords[6] = F;
+        chords[7] = G;
 
         // Create ArrayAdapter with all the chords
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context, R.array.chords_array, R.layout.support_simple_spinner_dropdown_item);
@@ -133,6 +163,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
+                Log.d("SnapRiffs", "Item selected: " + s1.getSelectedItem().toString());
                 selectedChord1 = getChordFromString(s1.getSelectedItem().toString());
             }
 
@@ -145,6 +176,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
+                Log.d("SnapRiffs", "Item selected: " + s2.getSelectedItem().toString());
                 selectedChord2 = getChordFromString(s2.getSelectedItem().toString());
             }
 
@@ -157,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
+                Log.d("SnapRiffs", "Item selected: " + s3.getSelectedItem().toString());
                 selectedChord3 = getChordFromString(s3.getSelectedItem().toString());
             }
 
@@ -169,6 +202,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> arg0, View arg1,
                                        int arg2, long arg3) {
+                Log.d("SnapRiffs", "Item selected: " + s4.getSelectedItem().toString());
                 selectedChord4 = getChordFromString(s4.getSelectedItem().toString());
             }
 
@@ -183,86 +217,135 @@ public class MainActivity extends AppCompatActivity {
         final Button b3 = (Button) findViewById(R.id.button3);
         final Button b4 = (Button) findViewById(R.id.button4);
 
+        // Set button transparent
+        b1.setBackgroundColor(Color.TRANSPARENT);
         b1.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Tell program, button one is clicked
-                    isClicked1 = true;
-
-                    // And no other buttons are clicked
-                    isClicked2 = false;
-                    isClicked3 = false;
-                    isClicked4 = false;
+                Log.d("SnapRiffs", "1 is clicked");
+                switch(event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Tell program, button one is clicked
+                        isClicked1 = true;
+                        Log.d("SnapRiffs", "down");
+                        // And no other buttons are clicked
+                        isClicked2 = false;
+                        isClicked3 = false;
+                        isClicked4 = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("SnapRiffs", "up");
+                        isClicked1 = false;
+                        break;
                 }
-                else if (event.getAction() == MotionEvent.ACTION_UP)
-                    isClicked1 = false;
-
+                //playChord();
                 return false;
             }
         });
+
+        // Set button transparent
+        b2.setBackgroundColor(Color.TRANSPARENT);
         b2.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Tell program, button two is clicked
-                    isClicked2 = true;
-
-                    // And no other buttons are clicked
-                    isClicked1 = false;
-                    isClicked3 = false;
-                    isClicked4 = false;
+                Log.d("SnapRiffs", "2 is clicked");
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Tell program, button two is clicked
+                        isClicked2 = true;
+                        Log.d("SnapRiffs", "down");
+                        // And no other buttons are clicked
+                        isClicked1 = false;
+                        isClicked3 = false;
+                        isClicked4 = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("SnapRiffs", "up");
+                        isClicked2 = false;
+                        break;
                 }
-                else if (event.getAction() == MotionEvent.ACTION_UP)
-                    isClicked2 = false;
-
+                //playChord();
                 return false;
             }
         });
-        b3.setOnTouchListener(new View.OnTouchListener()
-        {
+
+        // Set button transparent
+        b3.setBackgroundColor(Color.TRANSPARENT);
+        b3.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Tell program, button three is clicked
-                    isClicked3 = true;
-
-                    // And no other buttons are clicked
-                    isClicked1 = false;
-                    isClicked2 = false;
-                    isClicked4 = false;
+                Log.d("SnapRiffs", "3 is clicked");
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Tell program, button three is clicked
+                        isClicked3 = true;
+                        Log.d("SnapRiffs", "down");
+                        // And no other buttons are clicked
+                        isClicked1 = false;
+                        isClicked2 = false;
+                        isClicked4 = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("SnapRiffs", "up");
+                        isClicked3 = false;
+                        break;
                 }
-                else if (event.getAction() == MotionEvent.ACTION_UP)
-                    isClicked3 = false;
-
+                //playChord();
                 return false;
             }
 
         });
-        b4.setOnTouchListener(new View.OnTouchListener()
-        {
+
+        // Set button transparent
+        b4.setBackgroundColor(Color.TRANSPARENT);
+        b4.setOnTouchListener(new View.OnTouchListener() {
 
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Tell program, button four is clicked
-                    isClicked4 = true;
-
-                    // And no other buttons are clicked
-                    isClicked1 = false;
-                    isClicked2 = false;
-                    isClicked3 = false;
+                Log.d("SnapRiffs", "4 is clicked");
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Tell program, button four is clicked
+                        isClicked4 = true;
+                        Log.d("SnapRiffs", "down");
+                        // And no other buttons are clicked
+                        isClicked1 = false;
+                        isClicked2 = false;
+                        isClicked3 = false;
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        Log.d("SnapRiffs", "up");
+                        isClicked4 = false;
+                        break;
                 }
-                else if (event.getAction() == MotionEvent.ACTION_UP)
-                    isClicked4 = false;
-
+                //playChord();
                 return false;
             }
         });
 
         // Pebble stuff
-        boolean isConnected = PebbleKit.isWatchConnected(context);
+
+        // If we don't have a data receiver for the pebble, create one and register it
+        if(mDataReceiver == null) {
+            mDataReceiver = new PebbleKit.PebbleDataReceiver(APP_UUID) {
+
+                // If we get a message, ACK the message and play the current chord
+                // the pebble app only sends a message if there's a tap
+                @Override
+                public void receiveData(Context context, int transactionId, PebbleDictionary dict) {
+                    // Always ACK the message. Communicate!
+                    PebbleKit.sendAckToPebble(context, transactionId);
+                    Log.d("Pebble", "Got message from Pebble!");
+
+                    playChord();
+                }
+
+            };
+        }
+
+        // Register the data handler
+        PebbleKit.registerReceivedDataHandler(getApplicationContext(), mDataReceiver);
     }
 
     @Override
