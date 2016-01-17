a1dfe80281939acc1fb9d58fec40a898a720602b
Joseph Buchoff <joseph.buchoff@rutgers.edu>
2016-01-17 01:23:57 -0500

    Finished layout and Chord class

--- app/src/main/java/com/example/mlh_user/snapriffs/MainActivity.java
+++ app/src/main/java/com/example/mlh_user/snapriffs/MainActivity.java
@@ -1,13 +1,101 @@
 package com.example.mlh_user.snapriffs;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
+    // Initialize the chords
+    Chord A = new Chord("A", R.raw.A_chord);
+    Chord Am = new Chord("Am", R.raw.Am_chord);
+    Chord C = new Chord("C", R.raw.C_chord);
+    Chord D = new Chord("D", R.raw.D_chord);
+    Chord E = new Chord("E", R.raw.E_chord);
+    Chord Em = new Chord("Em", R.raw.Em_chord);
+    Chord F = new Chord("F", R.raw.F_chord);
+    Chord G = new Chord("G", R.raw.G_chord);
+
+    // Declare the buttons
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+
+    // Initialize the spinners
+    Spinner s1 = (Spinner) findViewById(R.id.spinner);
+    Spinner s2 = (Spinner) findViewById(R.id.spinner2);
+    Spinner s3 = (Spinner) findViewById(R.id.spinner3);
+    Spinner s4 = (Spinner) findViewById(R.id.spinner4);
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getApplicationContext(), R.array.chords_array, R.layout.support_simple_spinner_dropdown_item);=[
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        s1.setAdapter(adapter);
+        s2.setAdapter(adapter);
+        s3.setAdapter(adapter);
+        s4.setAdapter(adapter);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.mlh_user.snapriffs/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.mlh_user.snapriffs/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
