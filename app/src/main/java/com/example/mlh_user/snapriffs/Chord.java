8dc860d40550e549fd1e0428abb12b5f3f0b5563
Joseph Buchoff <joseph.buchoff@rutgers.edu>
2016-01-16 21:33:16 -0500

    How about this

---
+++ app/src/main/java/com/example/mlh_user/snapriffs/Chord.java
@@ -0,0 +1,8 @@
+package com.example.mlh_user.snapriffs;
+
+/**
+ * Created by MLH-User on 1/16/2016.
+ */
+public class Chord {
+
+}
---
+++ app/src/main/java/com/example/mlh_user/snapriffs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mlh_user.snapriffs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
